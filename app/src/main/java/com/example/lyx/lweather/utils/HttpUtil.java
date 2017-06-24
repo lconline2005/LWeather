d8a938c52cbeb39ad8b9e6ade73cdfbc3b63bad3
lyx <353989181@qq.com>
2017-06-24 20:42:45 +0800

    test commit

--- app/src/main/java/com/example/lyx/lweather/utils/HttpUtil.java
+++ app/src/main/java/com/example/lyx/lweather/utils/HttpUtil.java
@@ -12,5 +12,6 @@ public class HttpUtil {
         OkHttpClient client=new OkHttpClient();
         Request request=new Request.Builder().url(address).build();
         client.newCall(request).enqueue(callback);
+
     }
 }
